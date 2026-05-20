92ecb8b6356bd05dd50a2ffcdbfc664df2a9013e
thandc <chithanh040804@gmail.com>
2026-05-20 15:59:52 +0000

    test: share library

---
+++ vars/testConnection.groovy
@@ -0,0 +1,7 @@
+// File: vars/testConnection.groovy
+def call(String yourName) {
+    echo "===================================================="
+    echo "🎉 HELLO ${yourName}!"
+    echo "✅ KẾT NỐI SHARED LIBRARY THÀNH CÔNG RỒI NHÉ!"
+    echo "===================================================="
+}
