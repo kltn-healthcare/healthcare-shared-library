10fe27eb6ad759539d2aca9fb979215672686ce0
thandc <chithanh040804@gmail.com>
2026-05-21 09:44:01 +0000

    feat: adjust deployManifesst file

--- vars/deployManifest.groovy
+++ vars/deployManifest.groovy
@@ -1,3 +1,21 @@
-def call(String envName, String changedServices, String imageTag, String commitMessage) {
-  sh "./scripts/update-k8s-manifest.sh ${envName} ${changedServices} ${imageTag} '${commitMessage}'"
+def call(String scriptPath, String envName, String changedServices, String imageTag, String commitMessage) {
+    if (!changedServices?.trim()) {
+        echo "Bỏ qua Deploy do không có thay đổi mã nguồn."
+        return
+    }
+
+    echo "🚀 Bắt đầu Deploy Kustomize Manifest cho môi trường: [${envName.toUpperCase()}]"
+    
+    withEnv([
+        "ENVIRONMENT=${envName}",
+        "SERVICES=${changedServices}",
+        "IMAGE_TAG=${imageTag}",
+        "COMMIT_MESSAGE=${commitMessage}" // <-- BƠM BIẾN NÀY XUỐNG BASH
+    ]) {
+        sh """
+            export GIT_AUTH_URL=\$(echo \${MANIFEST_REPO_URL} | sed "s|https://|https://\${TOKEN}@|")
+            chmod +x ${scriptPath}
+            ${scriptPath}
+        """
+    }
 }
